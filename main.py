3250d7b0fbd860cabf4ea3b6c7889b7d191f128b
Kay de Blaauw <2063375blaauw@zuyd.nl>
2025-06-16 12:43:39 +0200

    Stash file for now

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the OT-Demo selection menu, please select an option: ")
+print("[1]: Send a HTTP-POST request to the Siemens portal")
+print("[2]: Run the Scanning script with added functionality to always turn the PLC off when its status is ""RUNNING""")
+print("[3]: Run the Automated scanning script with added functionality to always turn the PLC off when its status is ""RUNNING"", this does not require user input")
